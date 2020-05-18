b922f90efe9c1ba312111a5793beb3983ec546d3
zoeleeee <zhuzby@outlook.com>
2020-05-18 19:09:50 +0800

    P#indentation

--- mnist_noise.py
+++ mnist_noise.py
@@ -15,26 +15,26 @@ class AFT_Net(nn.Module):
         self.nb_layer = 6
 
     def set_layer_id(self, layer_id):
-    	self.layer_id == layer_id
+        self.layer_id == layer_id
 
     def forward(self, x):
-    	layer_id = self.layer_id
-    	# for i in range(layer_id, nb_layer)
-    	while self.nb_layer > layer_id:
-    		if layer_id == 0:
-    			x = F.relu(self.conv1(x))
-    		elif layer_id == 1:
-    			x = F.pool1(x)
-    		elif layer_id == 2:
-    			x = F.relu(self.conv2(x))
-    		elif layer_id == 3:
-    			x = self.pool2(x)
-    		elif layer_id == 4:
-    			x = x.view(-1, 3136)
-    			x = F.relu(self.fc1(x))
-    		elif layer_id == 5:
-    			x = self.fc2(x)
-    		layer_id += 1
+        layer_id = self.layer_id
+        # for i in range(layer_id, nb_layer)
+        while self.nb_layer > layer_id:
+            if layer_id == 0:
+                x = F.relu(self.conv1(x))
+            elif layer_id == 1:
+                x = F.pool1(x)
+            elif layer_id == 2:
+                x = F.relu(self.conv2(x))
+            elif layer_id == 3:
+                x = self.pool2(x)
+            elif layer_id == 4:
+                x = x.view(-1, 3136)
+                x = F.relu(self.fc1(x))
+            elif layer_id == 5:
+                x = self.fc2(x)
+            layer_id += 1
         return x
 
 class Net(nn.Module):
@@ -49,7 +49,7 @@ class Net(nn.Module):
         # self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-    	x0 = F.relu(self.conv1(x))
+        x0 = F.relu(self.conv1(x))
         x1 = self.pool1(x0)
         x2 = F.relu(self.conv2(x1))
         x3 = self.pool2(x2)
@@ -60,21 +60,21 @@ class Net(nn.Module):
         return [x0, x1, x2, x3, x4, x5]
 
 if __name__ == '__main__':
-	import sys
-	file = sys.argv[-1]
-	net = Net()
-	net.load_state_dict(torch.load(file))
-	aft = AFT_Net()
-	aft.load_state_dict(torch.load(file))
+    import sys
+    file = sys.argv[-1]
+    net = Net()
+    net.load_state_dict(torch.load(file))
+    aft = AFT_Net()
+    aft.load_state_dict(torch.load(file))
 
-	import numpy as np
-	data = np.load('data/mnist_data.npy').astype(np.float32) / 255.
-	inputs = torch.tensor(data[:1])
+    import numpy as np
+    data = np.load('data/mnist_data.npy').astype(np.float32) / 255.
+    inputs = torch.tensor(data[:1])
 
-	outs = net(inputs)
-	print(outs[-1])
-	for i,out in enumerate(outs):
-		aft.set_layer_id(i)
-		val = aft(out)
-		if not torch.eq(val, outs[-1]):
-			print(val, outs[-1]-val)
+    outs = net(inputs)
+    print(outs[-1])
+    for i,out in enumerate(outs):
+        aft.set_layer_id(i)
+        val = aft(out)
+        if not torch.eq(val, outs[-1]):
+            print(val, outs[-1]-val)
